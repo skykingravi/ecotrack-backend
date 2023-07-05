2738de938afff19cf3cd3d7aa1097c2e458d2730
Ravi Meena <97457969+skykingravi@users.noreply.github.com>
2023-07-05 15:54:12 +0530

    Update forms.js

--- routes/forms.js
+++ routes/forms.js
@@ -254,6 +254,7 @@ router.get("/waterPollutionForm/data/:secret", async (req, res) => {
 });
 router.get("/airPollutionForm/data/:secret", async (req, res) => {
     const { secret } = req.params;
+    console.log(secret);
     if (secret !== process.env.SECRET) {
         res.json({ message: "You are not Authorized!" });
     } else {
